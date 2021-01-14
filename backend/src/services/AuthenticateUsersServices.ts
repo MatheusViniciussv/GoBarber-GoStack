0b000680039fdbe45c3f9248f1d5b4bb00fa5520
Matheus Vinicius <matheusvinicius123466@gmail.com>
2021-01-14 13:45:48 -0300

    create JWT token

--- backend/src/services/AuthenticateUsersServices.ts
+++ backend/src/services/AuthenticateUsersServices.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
       throw Error('Incorrect email/password combination');
     }
 
+    const token = sign({}, '1862f6b70ff6dd7d180619e6c513fcb5', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
     return {
       user,
+      token
     };
   }
 }
