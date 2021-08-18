1601c54ea2409c8012bca68c25eec39a3dd29d40
Matheus_SESH <matheusvinicius123466@gmail.com>
2021-08-18 10:18:58 -0300

    refactor: button and input components

--- web/src/pages/SignIn/styles.ts
+++ web/src/pages/SignIn/styles.ts
@@ -30,46 +30,6 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    input {
-      background: #232129;
-      border-radius: 10px;
-      border: 2px solid #232129;
-      color: #f4ede8;
-
-      padding: 16px;
-      width: 100%;
-
-      & + input {
-        margin-top: 8px;
-      }
-
-      &::placeholder {
-        color: #666360;
-      }
-    }
-
-    button {
-      height: 56px;
-
-      background: #ff9000;
-      border-radius: 10px;
-      border: 0;
-      color: #312e38;
-
-      padding: 0 16px;
-      width: 100%;
-
-      font-weight: 500;
-
-      margin-top: 16px;
-
-      transition: background-color 0.2s;
-
-      &:hover {
-        background: ${shade(0.2, '#ff9000')};
-      }
-    }
-
     a {
       color: #f4ede8;
       display: block;
