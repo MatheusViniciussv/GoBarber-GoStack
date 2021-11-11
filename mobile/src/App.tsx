3f7e8240f72c6e10a49162b5c09ecf0dd69682dd
Matheus Vinicius <matheusvinicius123466@gmail.com>
2021-11-11 12:05:45 -0300

    user auth

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -5,15 +5,19 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
+import AppProvider from './hooks';
+
 import Routes from './routes';
 
 const App: React.FC = () => (
   <GestureHandlerRootView style={{ flex: 1 }}>
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-        <Routes />
-      </View>
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+          <Routes />
+        </View>
+      </AppProvider>
     </NavigationContainer>
   </GestureHandlerRootView>
 );
