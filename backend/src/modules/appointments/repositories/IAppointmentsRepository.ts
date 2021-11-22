fd812e690c4c2b186844d58563574170d2314f00
Matheus Vinicius <matheusvinicius123466@gmail.com>
2021-11-22 10:17:21 -0300

    rewriting respositories

--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,5 +1,7 @@
-import Appointments from '@modules/appointments/infra/typeorm/entities/Appointment';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmetDTO';
 
 export default interface IAppointmentsRepository {
-  findByDate(date: Date): Promise<Appointments | undefined>;
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
+  findByDate(date: Date): Promise<Appointment | undefined>;
 }
