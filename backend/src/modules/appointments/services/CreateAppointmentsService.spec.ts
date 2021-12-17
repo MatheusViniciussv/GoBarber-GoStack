20bda8a56df9516e18717a6b5eb88e235dfc26ab
Matheus Vinicius <matheusvinicius123466@gmail.com>
2021-12-16 21:12:07 -0300

    first test

--- backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,7 +1,19 @@
+import FakeAppointmentRepository from '../repositories/fake/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new Appointment', () => {
-    expect()
+  it('should be able to create a new Appointment', async () => {
+    const faceAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      faceAppointmentRepository,
+    );
+
+    const appointment = await createAppointmentService.execute({
+      date: new Date(),
+      provider_id: '123123',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
   });
 });
