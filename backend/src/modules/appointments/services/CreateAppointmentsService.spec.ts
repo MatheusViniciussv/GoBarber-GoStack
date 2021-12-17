33c4e8a719c7c529c5ac85838e4c3d5ed4ad4f5c
Matheus Vinicius <matheusvinicius123466@gmail.com>
2021-12-16 22:10:27 -0300

    appointment on the same date test

--- backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentsService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentRepository from '../repositories/fake/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -16,4 +17,27 @@ describe('CreateAppointment', () => {
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123123');
   });
+
+  it('should be able to create two Appointment on the same date', async () => {
+    const faceAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointment = new CreateAppointmentService(
+      faceAppointmentRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
+
+// 2.59
